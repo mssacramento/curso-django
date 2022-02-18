e5bbf4d3aed4054a898459f3da18b624d66d2b9a
Maiara Sacramento <97815531+mssacramento@users.noreply.github.com>
2022-02-18 17:15:35 -0300

    Update settings.py

--- pypro/settings.py
+++ pypro/settings.py
@@ -177,12 +177,12 @@ SENTRY_DSN = config('SENTRY_DSN', defalt=None)
 if SENTRY_DSN:
     sentry_sdk.init(dsn=SENTRY_DSN, integrations=[DjangoIntegration()],
 
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        # We recommend adjusting this value in production.
-        traces_sample_rate=1.0,
+                    # Set traces_sample_rate to 1.0 to capture 100%
+                    # of transactions for performance monitoring.
+                    # We recommend adjusting this value in production.
+                    traces_sample_rate=1.0,
 
-        # If you wish to associate users to errors (assuming you are using
-        # django.contrib.auth) you may enable sending PII data.
-        send_default_pii=True
-    )
+                    # If you wish to associate users to errors (assuming you are using
+                    # django.contrib.auth) you may enable sending PII data.
+                    send_default_pii=True
+                    )
